b76fc060721840e6d00498be51e64e1f9e9ea1fb
Zouquan0318 <128198664+Zouquan0318@users.noreply.github.com>
2024-11-18 14:56:37 +0800

    Update obs_browser.py
    上传到固定桶下面的文件夹中，但是文件路径需要手动写入

--- obs_browser.py
+++ obs_browser.py
@@ -1,27 +1,37 @@
 from obs import ObsClient
-from obs import const
+import os
+import traceback
 
 # 配置你的华为云 OBS 访问密钥
 access_key_id = 'U4C6ATAN6PEAR09O9JYT'
 secret_access_key = 'CC1FWzGIdhtgjV3YYQsfmXD70q8VIoB663sq8mvJ'
-server = 'http://obs.cn-north-1.myhuaweicloud.com'
+server = 'https://obs.cn-north-1.myhuaweicloud.com'  # 确保没有HTML实体
 
 # 创建 OBS 客户端
-obs_client = ObsClient(access_key_id, secret_access_key, server)
+print("Server URL:", server)
+obsClient = ObsClient(access_key_id, secret_access_key, server=server)
 
 # 配置桶和对象信息
-bucket_name = 'test-pocc'
-object_key = 'obs://test-pocc/dtc_demo/json_file_path_v402.json'  # 例如 'folder/subfolder/filename.json'
-file_path = 'C:/Users/awosh/OneDrive/Desktop/json_file_path_v402.json'  # 例如 'DTC_20241113.JSON'
+try:
+    bucketName = 'test-pocc'
+    objectKey = 'dtc_demo/json_file_path_v402.json'  # 例如 'folder/subfolder/filename.json'
+    file_path = 'C:/Users/awosh/OneDrive/Desktop/json_file_path_v402.json'  # 例如 'DTC_20241113.JSON'
 
 # 上传文件
-def upload_file(bucket_name, object_key, file_path):
-    try:
-        with open(file_path, 'rb') as file_data:
-            obs_client.put_object(bucket_name, object_key, file_data)
-        print(f"文件 {file_path} 上传到 OBS 桶 {bucket_name} 成功")
-    except Exception as e:
-        print(f"上传文件到 OBS 失败：{e}")
+    resp = obsClient.putFile(bucketName, objectKey, file_path)
+    # 返回码为2xx时，接口调用成功，否则接口调用失败
+    if resp.status < 300:
+        print('Put File Succeeded')
+        print('requestId:', resp.requestId)
+        print('etag:', resp.body.etag)
+        print('versionId:', resp.body.versionId)
+        print('storageClass:', resp.body.storageClass)
+    else:
+        print('Put File Failed')
+        print('requestId:', resp.requestId)
+        print('errorCode:', resp.errorCode)
+        print('errorMessage:', resp.errorMessage)
 
-# 调用上传函数
-upload_file(bucket_name, object_key, file_path)
+except:
+    print('Put File Failed')
+    print(traceback.format_exc())
